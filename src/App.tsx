80ee6bcc80afbda715900c31fc94b421a26268c9
UEBSamuelHoyos <146367592+UEBSamuelHoyos@users.noreply.github.com>
2026-05-04 17:52:11 -0500

    fix Confirmar casing

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import Confirmar from "./pages/confirmar";
+import Confirmar from "./pages/Confirmar";
 const queryClient = new QueryClient();
 
 const App = () => (
