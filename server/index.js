4e2714925acb0c60c4ff3372f91fc641fb77b057
UEBSamuelHoyos <146367592+UEBSamuelHoyos@users.noreply.github.com>
2026-05-04 17:57:47 -0500

    fix railway port

--- server/index.js
+++ server/index.js
@@ -92,6 +92,7 @@ app.post("/send-calendly", async (req, res) => {
   }
 });
 
-app.listen(3001, () => {
-  console.log("Servidor corriendo en http://localhost:3001");
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
 });
